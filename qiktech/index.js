ac2709094c37144ed4d9fa3c3083d5b6abfeae85
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-07-06 02:31:39 +0530

    Huge Change

--- qiktech/index.js
+++ qiktech/index.js
@@ -5,3 +5,25 @@ const scroll = new LocomotiveScroll({
      el: document.querySelector('[data-scroll-container]'),
      smooth: true
  });
+
+ 
+ 
+//  Gallery
+const buttons = document.querySelectorAll('.project');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
+
+function open(e) {
+  overlay.classList.add('open');
+  const src= e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+buttons.forEach(button => button.addEventListener('click', open));
+overlay.addEventListener('click', close);
+
+
